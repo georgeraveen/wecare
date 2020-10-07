d1148274ba6f6ab77315d6bc147885709034fb16
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2020-10-07 16:30:40 -0700

    Create viewPendingQueue.php
    create viewPendingQueue

---
+++ doctor/viewPendingQueue.php
@@ -0,0 +1,19 @@
+<?php
+require_once("./../config/config.php");
+
+if (!isset($_SESSION["user_id"]) || $_SESSION["user_id"] == "") {
+    // not logged in send to login page
+    redirect("./../index.php");
+}
+ if($_SESSION["rolecode"]!="DOC"){
+    die("You dont have the permission to access this page");
+ }
+
+
+
+ include './../header.php';
+?>
+
+
+<link rel="stylesheet" href= "./../css/home.css">
+<link rel="stylesheet" href= "./../css/style.css">
