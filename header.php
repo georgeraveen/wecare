19dea8c6c44a0da189b81df96223e0b529928820
georgeraveen <georgeraveen@gmail.com>
2020-10-09 00:11:50 +0530

    Update header.php

--- header.php
+++ header.php
@@ -5,11 +5,21 @@
   <title>WeCare Life</title>
 </head>
 <body>
+<?php
+
+session_start();
+if($_SESSION["rolecode"] =='CUST'){
+  $home="./../customer.php";
+}
+else{
+  $home="./../";
+}
+?>
 <div class="fixed-header">
     <div class="containerHeader">
         <nav>
-            <a href="./../"><img class="logo" src="./../images/logo.png"></a>
-            <a href="./../"><div>
+            <a href= <?php echo "\"$home\"";?>><img class="logo" src="./../images/logo.png"></a>
+            <a href= <?php echo "\"$home\"";?>><div>
               Welcome to WeCare 
               <?php   
                 if($_SESSION["rolecode"] =='DEO'){
