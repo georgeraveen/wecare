b202210f6c2e051dfb7ae0e770ad6ccb70988ddc
Thusiya <56513558+Thusiya05@users.noreply.github.com>
2020-10-15 17:58:39 +0530

    managerHome done

--- app/views/manager/viewreports.php
+++ app/views/manager/viewreports.php
@@ -1,17 +1,5 @@
-<?php
-require_once("./../config/config.php");
-if (!isset($_SESSION["user_id"]) || $_SESSION["user_id"] == "") {
-  // not logged in send to login page
-  redirect("./../index.php");
-}
-if($_SESSION["rolecode"]!="MGR"){
-  die("You dont have the permission to access this page");
-}
-include './../header.php';
-?>
-
-<link rel="stylesheet" href= "./../css/home.css">
-<link rel="stylesheet" href= "./../css/style.css">
+<link rel="stylesheet" href= "./../../css/home.css">
+<link rel="stylesheet" href= "./../../css/style.css">
 <div class="containers">
   <h1>View Reports</h1><br>
     <div class="form-container">
