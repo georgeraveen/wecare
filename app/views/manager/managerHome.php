bacf3c78951067e56439d9c49316fc0acce36ebb
Thusiya <56513558+Thusiya05@users.noreply.github.com>
2020-10-17 00:22:19 +0530

    Update insurance policy page

--- app/views/manager/managerHome.php
+++ app/views/manager/managerHome.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href= "./../../css/home.css">
 <div class="grid-container">
   <a class="grid-item" href="./../viewReports">View reports</a>
-  <a class="grid-item" href="#">Update insurance policy</a>
+  <a class="grid-item" href="./../insurancePolicy">Update insurance policy</a>
   <a class="grid-item" href="#">Update weekly roster</a>  
   <a class="grid-item" href="#">Create new employee account</a>
   <a class="grid-item" href="#">Manage employee accounts</a>
