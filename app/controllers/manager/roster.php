04b3ae6d45a1d8b2ea9a15215675e2f3ae2f8b6e
georgeraveen <georgeraveen@gmail.com>
2020-11-08 15:37:48 +0530

    roster update

--- app/controllers/manager/roster.php
+++ app/controllers/manager/roster.php
@@ -11,9 +11,10 @@ class Roster extends Controller{
     }
     public function createNew(){
         $this->checkPermission("MGR");
-        include './../app/header.php';
+        // include './../app/header.php';
         $this->view('manager/createRoster');
         include './../app/footer.php';
         // echo "asas";
     }
+    
 }
--- app/views/manager/createRoster.php
+++ app/views/manager/createRoster.php
@@ -1,18 +1,85 @@
 <link rel="stylesheet" href= "./../../css/home.css">
+<!-- <link rel="stylesheet" href= "./../../css/modal.css"> -->
 <link rel="stylesheet" href= "./../../css/style.css">
 <div class="containers">
   <h1>Create New Roster</h1><br>
   <div class="table-container">
-    <table class="table-view">
-      <tr>
-        <th>Employee ID</th>
-        <th>Employee Name</th>
-        <th>No of days</th>
-        <th>No of cases</th>
-        <th>No of overpaments</th>
-        <th>Remark</th>
-      </tr>
+    <table id="rosterTable" class="table-view">
+      <thead>
+        <th>Time Slot</th>
+        <th>Monday</th>
+        <th>Tuesday</th>
+        <th>Wednesday</th>
+        <th>Thursday</th>
+        <th>Friday</th>
+        <th>Saturday</th>
+        <th>Sunday</th>
+      </thead>
+      <tbody>
+        <tr id="slot1">
+          <td id="time1">00:00H - 08:00H</td>
+          <?php
+            for($i=1;$i<=7;$i++){
+              echo "<td><button id=\"1".$i."\" class=\"editBtn\" onClick=\"addUser(this.id)\" >Add user</button></td>";
+            }
+          ?>
+        </tr>
+        <tr id="slot2">
+          <td id="time2">08:00H - 16:00H</td>
+          <?php
+            for($i=1;$i<=7;$i++){
+              echo "<td><button id=\"2".$i."\" class=\"editBtn\" onClick=\"addUser(this.id)\" >Add user</button></td>";
+            }
+          ?>
+        </tr>
+        <tr id="slot3">
+          <td id="time3">16:00H - 00:00H</td>
+          <?php
+            for($i=1;$i<=7;$i++){
+              echo "<td><button id=\"3".$i."\" class=\"editBtn\" onClick=\"addUser(this.id)\" >Add user</button></td>";
+            }
+          ?>
+        </tr>
+        <tr id="slotBlank">
+          <td ></td>
+          <?php
+            for($i=1;$i<=7;$i++){
+              echo "<td></td>";
+            }
+          ?>
+        </tr>
+      </tbody>
     </table>
     
   </div>
 </div>
+<div id="Modal" class="modal">
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">&times;</span>
+            <input id="btnIDModel" type="text">
+      <div class="row">
+        <div class="column">
+          <div class="formInput">
+            <label for="newMedID">Type</label><br>
+            <select id="newMedID" name="newMedID">
+              <option value=a>a</option>
+              <option value=b>b</option>
+              <option value=c>c</option>
+              <option value=d>d</option>
+            </select><br>
+          </div>
+          
+        </div>
+        <div class="column">
+          <div class="formInput">
+            <button class="editBtn" onClick="addToTable()">
+            Add
+            </button>
+          </div>
+          
+        </div>
+      </div>
+     
+</div>
+<script type="text/javascript" src="./../../js/roster1.js"></script>
