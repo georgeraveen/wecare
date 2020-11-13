ded314ad205534e43162d1aa49d5b8c2bbccf93f
Buddhi Wickramasinghe <chathuranga4742@gmail.com>
2020-11-13 08:08:48 +0530

    baseline files done

---
+++ app/controllers/medScru/viewCompletedCases.php
@@ -0,0 +1,12 @@
+<?php
+
+class viewCompletedCases extends Controller{
+    
+    public function index(){
+        $this->checkPermission("MED");
+        include './../app/header.php';
+        $this->view('medScru/completedCases');
+        include './../app/footer.php';
+        // echo "asas";
+    }
+}
--- app/views/medScru/medScruHome.php
+++ app/views/medScru/medScruHome.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href= "./../../css/home.css">
 <div class="grid-container">
-    <a class="grid-item" href="./medScruPendingQueue.php">View my pending cases queue</a>
-    <a class="grid-item" href="./medScruCompleted.php">View my completed cases queue</a>
-    <a class="grid-item" href="#">Manage medical details of customers</a>
-    <a class="grid-item" href="./caseView.php">View weekly roster</a> 
+    <a class="grid-item" href="./../viewPendingCases/index">View my pending cases queue</a>
+    <a class="grid-item" href="./../viewCompletedCases/index">View my completed cases queue</a>
+    <a class="grid-item" href="./../manageCustMedical/index">Manage medical details of customers</a>
+    <a class="grid-item" href="./../viewRoster/index">View weekly roster</a> 
 </div>
