71b9f98b8475c90fa6f11469a84a196b654fddda
Buddhi Wickramasinghe <chathuranga4742@gmail.com>
2020-11-14 07:23:20 +0530

    view medical conditions

--- app/views/medScru/viewMedicalCondition.php
+++ app/views/medScru/viewMedicalCondition.php
@@ -2,7 +2,12 @@
 <link rel="stylesheet" href= "./../../css/home.css">
 <link rel="stylesheet" href= "./../../css/style.css">
 <div class="containers">
-  <h1>Past Health conditions of Customer ***Customer Name***, ***Cust ID****</h1>
+<div class="row">
+<div class="column">
+<input type="submit" id="newCondition" name="newCondition" class="btn-submit" value="New Condition"><br>
+</div> 
+</div>
+  <h1>Past Health conditions of Customer *Customer Name*, *Cust ID*</h1>
   <div class="table-container">
     <table class="table-view">
       <tr>
@@ -11,7 +16,15 @@
         <th>Date</th>
         <th>Type</th>
         <th>Health Condition</th>
-        <th>Cooments</th>
+        <th>Comments</th>
+      </tr>
+      <tr>
+        <td>007</td>
+        <td>00024</td>
+        <td>2020/11/10</td>
+        <td>Congenital</td>
+        <td>Lung Condition</td>
+        <td>Insert comment here when creating medical condiitons</td>
       </tr>
     </table>
     
