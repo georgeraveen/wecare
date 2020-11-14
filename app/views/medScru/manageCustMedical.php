32eb49b527d27c50dce662e4422a70b244f8616b
Buddhi Wickramasinghe <chathuranga4742@gmail.com>
2020-11-14 06:16:35 +0530

    Update manageCustMedical.php

--- app/views/medScru/manageCustMedical.php
+++ app/views/medScru/manageCustMedical.php
@@ -4,7 +4,7 @@
 <div class="containers">
   <h1>Select Customer Profile</h1><br>
   <div class="form-container2">
-    <form >
+    <form>
       <div class="row">
         <div class="column">
           <select id="custID" name="custID" required>
@@ -21,3 +21,47 @@
     </form>
   </div>
   <br><br>
+  <h1>Or search Customer Profile</h1>
+  <h2>Search by</h1>
+  <div class="form-container2">
+    <form>
+      <div class="row">
+        <div class="column">
+          <select id="custData" name="custData" required>
+            <option>Name</option>
+            <option>Customer ID</option>
+            <option>NIC</option>
+            <option>Phone Number</option>
+          </select>
+        </div>
+        <div class="column">
+          <div class="formInput">
+            <label for="custData"></label>
+            <input type="data" id="data" name="data" class="input">          
+          </div>
+        </div>
+        </div>
+        <div class="row">
+        <input type="submit" id="setOption" name="setOption" class="btn-submit" value="Search"><br>
+        </div>
+    </form><br>
+    </div>
+    <div class="table-container">
+    <table class="table-view">
+      <tr>
+        <th>Name</th>
+        <th>Customer ID</th>
+        <th>NIC</th>
+        <th>Phone Number</th>
+        <th>Action</th>
+      </tr>
+      <tr>
+        <td>Mr.Perera</td>
+        <td>001</td>
+        <td>9723797713V</td>
+        <td>07355322</td>
+      </tr>
+    </table>
+  </div>
+
+  
