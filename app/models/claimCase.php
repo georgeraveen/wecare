de4deec5aa992f83fb994cfcb37d386cc1c74a5f
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2020-11-13 23:03:49 -0800

    fieldAgent-ViewPendingQueue

--- app/models/claimCase.php
+++ app/models/claimCase.php
@@ -95,5 +95,85 @@ class ClaimCase extends Models{
 
         // echo $this->dataEntryOfficerID . $this->healthCondition;
     }
+<<<<<<< Updated upstream
+=======
+
+
+
+    //**************************************** FUNCTIONS OF DOCTOR **********************************************
+    //Docter-view pending queue
+    public function getDoctorList($doctorID){
+        // var_dump($this->conn);
+        $stmt= $this->conn->prepare("SELECT claimID, customer.custName,admitDate, CONCAT(employee.empFirstName, \" \", employee.empLastName) AS medSrcName , hospital.name
+        FROM claim_case
+        INNER JOIN customer
+            ON claim_case.custID=customer.custID
+        INNER JOIN hospital
+            ON claim_case.hospitalID=hospital.hospitalID
+        INNER JOIN employee
+            ON claim_case.medScruID=employee.empID
+        WHERE claim_case.doctorID = $doctorID;
+                    ");
+        
+        $stmt->execute();
+        return $stmt->fetchAll();
+
+    }
+    //update-docteor pending queue
+    public function update2($_id){
+        $stmt= $this->conn->prepare("update $this->table set admitDate= :admitDate, dischargeDate= :dischargeDate, icuFromDate= :icuFromDate, icuToDate= :icuToDate,
+                                                            healthCondition= :healthCondition, custID= :custID, medScruID= :medScruID, dataEntryOfficerID= :dataEntryOfficerID,
+                                                            FieldAgID= :FieldAgID, hospitalID= :hospitalID where claimID = $_id ") ;
+
+        $stmt -> bindParam(':admitDate', $this->admitDate );
+        $stmt -> bindParam(':dischargeDate', $this->dischargeDate); 
+        $stmt -> bindParam(':icuFromDate', $this->icuFromDate );
+        $stmt -> bindParam(':icuToDate', $this->icuToDate );
+        $stmt -> bindParam(':healthCondition', $this->healthCondition );
+        $stmt -> bindParam(':custID', $this->custID );
+        $stmt -> bindParam(':medScruID', $this->medScruID );
+        $stmt -> bindParam(':dataEntryOfficerID', $this->dataEntryOfficerID );
+        $stmt -> bindParam(':FieldAgID', $this->FieldAgID );
+        $stmt -> bindParam(':hospitalID', $this->hospital);
+        $stmt->execute();
+
+        // echo $this->dataEntryOfficerID . $this->healthCondition;
+    }
+    //doctor-view from pending queue
+    public function getCaseDetailsDoctor($claimID){
+        $stmt= $this->conn->prepare("SELECT customer.custName,claimID,admitDate,icuFromDate,dischargeDate,icuToDate,hospital.name,healthCondition 
+        FROM claim_case 
+        INNER JOIN hospital 
+            ON claim_case.hospitalID=hospital.hospitalID 
+        INNER JOIN customer
+            ON claim_case.custID=customer.custID 
+        WHERE claimID = $claimID");
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+
+
+//*********************************************** FUNCTIONS OF FIELD AGENT *********************************************
+//get details for table
+
+    public function getFieldAgList($fieldAgID){
+        // var_dump($this->conn);
+        $stmt= $this->conn->prepare("SELECT claimID, customer.custName,admitDate, CONCAT(employee.empFirstName, \" \", employee.empLastName) AS medSrcName , hospital.name ,caseStatus
+        FROM claim_case 
+        INNER JOIN customer
+            ON claim_case.custID=customer.custID 
+        INNER JOIN hospital 
+             ON claim_case.hospitalID=hospital.hospitalID 
+        INNER JOIN employee 
+            ON claim_case.medScruID=employee.empID
+        WHERE claim_case.fieldAgID = $fieldAgID;
+                    ");
+        
+        $stmt->execute();
+        return $stmt->fetchAll();
+
+}
+
+>>>>>>> Stashed changes
 }
 ?>
