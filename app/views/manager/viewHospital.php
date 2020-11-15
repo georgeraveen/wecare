c0c32c8f4df965df246d2202d44738312e86a22a
Thusiya <56513558+Thusiya05@users.noreply.github.com>
2020-11-15 12:27:56 +0530

    add new hospital

---
+++ app/views/manager/viewHospital.php
@@ -0,0 +1,29 @@
+<link rel="stylesheet" href= "./../../css/home.css">
+<link rel="stylesheet" href= "./../../css/style.css">
+<div class="containers">
+  <h1>Hospitals</h1><br>
+    <div class="form-container">
+
+        <form action="#" method="post">
+            <div class="row">
+                <div class="column">
+                    <div class="formInput">
+                        <label for="empFirstName">Hospital Name</label><br>
+                        <input type="text" id="empFirstName" name="empFirstName" required class="input"><br>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="formInput">
+                        <label for="empLastName">Contact Number</label><br>
+                        <input type="text" id="empLastName" name="empLastName" required class="input"><br>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="formInput">
+                        <input type="submit" id="newHospital" name="newHospital" class="btn-submit" value="Submit"><br>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
