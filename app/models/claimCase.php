3b56115f04a737ab9a3b66be92ec6bf18a07a3a3
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2020-11-14 22:10:37 -0800

    upadete - doctor

--- app/models/claimCase.php
+++ app/models/claimCase.php
@@ -133,36 +133,21 @@ class ClaimCase extends Models{
         $stmt->execute();
         return $stmt->fetchAll();
     }
-    public function setValueDoc($healthCondition,$doctorComment){
-        // echo $PadmitDate;
-        //$this->claimID=$PclaimID;
-        //$this->admitDate= !empty($PadmitDate) ? $PadmitDate : null;
-        //$this->dischargeDate =  !empty($PdischargeDate) ? $PdischargeDate : null;
-        //$this->icuFromDate= !empty($PicuToDate) ? $PicuToDate : null;
-        //$this->icuToDate= !empty($PicuToDate) ? $PicuToDate : null;
+    public function setValueDoc($doctorComment,$healthCondition){
+       
         $this->healthCondition= !empty($healthCondition) ? $healthCondition : null;
         $this->doctorComment= !empty($doctorComment) ? $doctorComment : null;
           
-       // $this->healthCondition= !empty($PhealthCondition) ? $PhealthCondition : null;
-       // $this->medScruID=$PmedScrut;
-        //$this->dataEntryOfficerID = $_SESSION["user_id"];
-        //$this->FieldAgID=$PfieldAg;
        
-        //$this->policyID=$PpolicyID;
     }
     public function updateSingleCaseDoc($_id){
         $stmt= $this->conn->prepare("update $this->table set  healthCondition= :healthCondition, doctorComment= :doctorComment,caseStatus='Doctor confirmed'
                                                             where claimID = $_id ") ;
     
-       // $stmt -> bindParam(':admitDate', $this->admitDate );
-        //$stmt -> bindParam(':dischargeDate', $this->dischargeDate); 
-        $stmt -> bindParam(':doctorComment', $this->doctorComment );
+        
         $stmt -> bindParam(':healthCondition', $this->healthCondition );
-        //$stmt -> bindParam(':custID', $this->custID );
-       // $stmt -> bindParam(':medScruID', $this->medScruID );
-       // $stmt -> bindParam(':dataEntryOfficerID', $this->dataEntryOfficerID );
-       // $stmt -> bindParam(':FieldAgID', $this->FieldAgID );
-        //$stmt -> bindParam(':hospitalID', $this->hospital);
+        $stmt -> bindParam(':doctorComment', $this->doctorComment );
+      
         $stmt->execute();
     
         // echo $this->dataEntryOfficerID . $this->healthCondition;
@@ -213,12 +198,7 @@ public function setValueFag($PclaimID,$PadmitDate,$PdischargeDate,$PicuFromDate,
     $this->icuFromDate= !empty($PicuToDate) ? $PicuToDate : null;
     $this->icuToDate= !empty($PicuToDate) ? $PicuToDate : null;
       
-   // $this->healthCondition= !empty($PhealthCondition) ? $PhealthCondition : null;
-   // $this->medScruID=$PmedScrut;
-    //$this->dataEntryOfficerID = $_SESSION["user_id"];
-    //$this->FieldAgID=$PfieldAg;
    
-    //$this->policyID=$PpolicyID;
 }
 //update single case details FAG
 public function updateSingleCaseFag($_id){
@@ -230,11 +210,7 @@ public function updateSingleCaseFag($_id){
     $stmt -> bindParam(':dischargeDate', $this->dischargeDate); 
     $stmt -> bindParam(':icuFromDate', $this->icuFromDate );
     $stmt -> bindParam(':icuToDate', $this->icuToDate );
-    //$stmt -> bindParam(':custID', $this->custID );
-   // $stmt -> bindParam(':medScruID', $this->medScruID );
-   // $stmt -> bindParam(':dataEntryOfficerID', $this->dataEntryOfficerID );
-   // $stmt -> bindParam(':FieldAgID', $this->FieldAgID );
-    //$stmt -> bindParam(':hospitalID', $this->hospital);
+   
     $stmt->execute();
 
     // echo $this->dataEntryOfficerID . $this->healthCondition;
