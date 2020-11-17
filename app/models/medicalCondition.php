470331b8a8bad41b004d33286476c9c7f6d67e34
Buddhi Wickramasinghe <chathuranga4742@gmail.com>
2020-11-17 13:25:13 +0530

    update

--- app/models/medicalCondition.php
+++ app/models/medicalCondition.php
@@ -1,23 +1,32 @@
 <?php
 class medicalCondition extends Models{
     private $conn;
-    private $table="customer";
+    private $table="med_record";
     private $custID;
-    private $custName;
-    private $custAddress;
-    private $custNIC;
-    private $custDOB;
-    private $email;
-    private $gender;
-    private $policyID;
-    private $custContact;
-
+    private $recordID;
+    private $date;
+    private $type;
+    private $healthCondition;
+    private $comments;
 
     public function __construct(){
         $this->conn=$this->dataConnect();
     }
     public function read(){
 
+    }
+    public function create(){
+        $stmt= $this->conn->prepare("insert into $this->table (custID,date,type,healthCondition,comments)
+                                                            values (:custID, :date, :type, :healthCondition, :comments)");
+        $stmt -> bindParam(':custID', $this->custID);
+        $stmt -> bindParam(':date', $this->date); 
+        $stmt -> bindParam(':type', $this->type);
+        $stmt -> bindParam(':icuToDate', $this->icuToDate);
+        $stmt -> bindParam(':healthCondition', $this->healthCondition);
+        $stmt -> bindParam(':comments', $this->comments);
+        $stmt->execute();
+
+        // echo $this->dataEntryOfficerID . $this->healthCondition;
     }
   
 
