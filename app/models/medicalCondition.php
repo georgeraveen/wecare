ed5bdfa0d729725f2c80e37a629bdc4dc7d2f045
Buddhi Wickramasinghe <chathuranga4742@gmail.com>
2020-11-17 11:16:34 +0530

    Create medicalCondition.php

---
+++ app/models/medicalCondition.php
@@ -0,0 +1,22 @@
+<?php
+class medicalCondition extends Models{
+    private $conn;
+    private $table="customer";
+    private $custID;
+    private $custName;
+    private $custAddress;
+    private $custNIC;
+    private $custDOB;
+    private $email;
+    private $gender;
+    private $policyID;
+    private $custContact;
+
+
+    public function __construct(){
+        $this->conn=$this->dataConnect();
+    }
+
+
+}
+?>
